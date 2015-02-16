e45b51501be7572f6506d2b0fbbfb1ef767362f7
consti <consti@consti.de>
2015-02-16 17:06:43 +0700

    Model: Member (Devise model)
    Required:
    first_name, last_name, email, password
    
    is:
    :database_authenticatable, :registerable,
    :recoverable, :rememberable, :trackable,
    :validatable, :confirmable

---
+++ app/models/member.rb
@@ -0,0 +1,9 @@
+class Member < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+
+  validates :first_name, :last_name, presence: true
+end
