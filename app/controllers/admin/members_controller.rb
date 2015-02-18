159b6602c92813079f4e4ea0b08fdb5afc9f25e4
consti <consti@consti.de>
2015-02-18 10:57:12 +0700

    REST Admin/members

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,69 @@ module Admin
   class MembersController < AdminController
     load_and_authorize_resource
 
+    before_action :allow_without_password, only: :update
+
     def index
+    end
+
+    def new
+      @member = Member.new
+    end
+
+    def create
+      @member = Member.new(member_params)
+      respond_to do |format|
+        if @member.save
+          format.html { redirect_to :back, notice: 'Member was successfully created.' }
+          format.json { render json: @member, status: :created }
+        else
+          format.html { redirect_to :back, alert: 'Error creating member.' }
+          format.json { render json: @member.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      @member.update(member_params)
+      respond_to do |format|
+        if @member.save
+          format.html { redirect_to :back, notice: 'Member was successfully updated.' }
+          format.json { render json: @member, status: :created }
+        else
+          format.html { redirect_to :back, alert: 'Error updating member.' }
+          format.json { render json: @member.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @member.destroy
+      respond_to do |format|
+        format.html { redirect_to :back, notice: 'Member was successfully deleted.' }
+        format.json { head :no_content }
+      end
+    end
+
+    private
+
+    def allow_without_password
+      if params[:member][:password].blank? && params[:member][:password_confirmation].blank?
+        params[:member].delete("password")
+        params[:member].delete("password_confirmation")
+      end
+    end
+
+    def member_params
+      params.require(:member).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation,
+        :role)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get :dashboard
     resources :companies
-    resources :members, only: [:index]
+    resources :members
   end
 
   root 'welcome#index'
