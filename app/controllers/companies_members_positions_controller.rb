8eeca805e7bb1642021c149793ee6310c9357434
consti <consti@consti.de>
2015-02-18 20:31:13 +0700

    auto submit CompaniesMembersPositions
    ..and remove ‘new’ view and logic

--- app/controllers/companies_members_positions_controller.rb
+++ app/controllers/companies_members_positions_controller.rb
@@ -2,10 +2,6 @@ class CompaniesMembersPositionsController < ApplicationController
   before_action :authenticate_member!
   load_and_authorize_resource
 
-  def new
-    @companies_members_position = CompaniesMembersPosition.new(member: current_member)
-  end
-
   def create
     @companies_members_position = CompaniesMembersPosition.new(companies_members_position_params)
     respond_to do |format|
@@ -13,7 +9,7 @@ class CompaniesMembersPositionsController < ApplicationController
         format.html { redirect_to @companies_members_position.company, notice: 'Position was successfully created but needs to be approved.' }
         format.json { render json: @companies_members_position, status: :created }
       else
-        format.html { render 'new', alert: 'Error creating position.' }
+        format.html { redirect_to @companies_members_position.company, alert: 'Error creating position.' }
         format.json { render json: @companies_members_position.errors, status: :unprocessable_entity }
       end
     end
