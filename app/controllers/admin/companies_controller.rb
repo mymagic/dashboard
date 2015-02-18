3335630c3636bd4bc925ea8c12c93439f2e6b1e7
consti <consti@consti.de>
2015-02-18 10:32:19 +0700

    REST Admin/companies

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -4,5 +4,52 @@ module Admin
 
     def index
     end
+
+    def new
+      @company = Company.new
+    end
+
+    def create
+      @company = Company.new(company_params)
+      respond_to do |format|
+        if @company.save
+          format.html { redirect_to :back, notice: 'Company was successfully created.' }
+          format.json { render json: @company, status: :created }
+        else
+          format.html { redirect_to :back, alert: 'Error creating company.' }
+          format.json { render json: @company.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      @company.update(company_params)
+      respond_to do |format|
+        if @company.save
+          format.html { redirect_to :back, notice: 'Company was successfully updated.' }
+          format.json { render json: @company, status: :created }
+        else
+          format.html { redirect_to :back, alert: 'Error updating company.' }
+          format.json { render json: @company.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @company.destroy
+      respond_to do |format|
+        format.html { redirect_to :back, notice: 'Company was successfully deleted.' }
+        format.json { head :no_content }
+      end
+    end
+
+    private
+
+    def company_params
+      params.require(:company).permit(:name)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get :dashboard
-    resources :companies, only: [:index]
+    resources :companies
     resources :members, only: [:index]
   end
 
