e4f372bd99dc7b9bea3c4971299577082bf2cd62
Jan Jones <jan.jones@oozou.com>
2015-02-19 15:38:12 +0700

    Added spec helpers and marked unimplemented tests

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending "Not implemented"
+  
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
--- spec/controllers/office_hours_controller_spec.rb
+++ spec/controllers/office_hours_controller_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 RSpec.describe OfficeHoursController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending "Not implemented"
 
-  describe "GET #book" do
-    it "returns http success" do
-      get :book
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #offer" do
-    it "returns http success" do
-      get :offer
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #book" do
+  #   it "returns http success" do
+  #     get :book
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #offer" do
+  #   it "returns http success" do
+  #     get :offer
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
