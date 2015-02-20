169e44ccecf02dfe13dbfe5cb041fa332ac157cf
consti <consti@consti.de>
2015-02-20 15:20:12 +0700

    don’t make priority_order a required attribute

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,15 +2,14 @@ class Position < ActiveRecord::Base
   include RankedModel
   ranks :priority_order
 
-  validates :name, :priority_order, presence: true
-  validates :name, uniqueness: :true
+  validates :name, presence: true, uniqueness: :true
 
   has_many(:companies_members,
            class: CompaniesMembersPosition,
            dependent: :destroy,
            inverse_of: :position)
-  has_many :companies, through: :companies_members
-  has_many :members,   through: :companies_members
+  has_many :companies, -> { uniq }, through: :companies_members
+  has_many :members, -> { uniq }, through: :companies_members
 
   scope :ordered, -> { rank(:priority_order) }
 end
