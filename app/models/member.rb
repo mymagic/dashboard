0813e2cb99161a15d5f654cb879d61cf972c312e
consti <consti@consti.de>
2015-03-05 12:57:26 +0700

    Re-add :registerable to enable 'edit member'

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,9 @@ class Member < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :validatable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable, :trackable, :registerable,
          :confirmable, validate_on_invite: true
 
   validates :first_name, :last_name, :time_zone, presence: true, on: :update
