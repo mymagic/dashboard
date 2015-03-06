e12a201a6ff680766bc3ce423f0e3e365afc6b8f
n-thongjor <nut@oozou.com>
2015-03-06 14:01:29 +0700

    Scope all routes by community

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,26 @@ Rails.application.routes.draw do
     invitations: 'invitations'
   }
 
-  resources :members, only: [:index, :show]
-  resources :companies, only: [:index, :show] do
-    resources :members, only: [:new, :create, :edit, :update]
-  end
-  resources :companies_members_positions, only: [:create]
-  resources :office_hours, only: [:index, :create, :destroy] do
-    patch 'book', on: :member
-    delete 'cancel', on: :member
-  end
-
-  namespace :admin do
-    get :dashboard
-    resources :companies
-    resources :office_hours
-    resources :members do
-      patch 'resend_invitation', on: :member
+  resources :communities do
+    resources :members, only: [:index, :show]
+    resources :companies, only: [:index, :show] do
+      resources :members, only: [:new, :create, :edit, :update]
     end
-    resources :positions
+    resources :companies_members_positions, only: [:create]
+    resources :office_hours, only: [:index, :create, :destroy] do
+      patch 'book', on: :member
+      delete 'cancel', on: :member
+    end
+
+    namespace :admin do
+      get :dashboard
+      resources :companies
+      resources :office_hours
+      resources :members do
+        patch 'resend_invitation', on: :member
+      end
+      resources :positions
+    end
   end
 
   root 'welcome#index'
