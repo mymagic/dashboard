290680b61805c284f2068f2374bf481636144b1b
n-thongjor <nut@oozou.com>
2015-03-25 13:04:43 +0700

    Set favicon as community logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def favicon_path
+    if current_community
+     current_community.logo.url
+    else
+       image_path('missing/community/icon_default.png')
+    end
+  end
 end
