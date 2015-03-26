8ea0238f7f4a98f33ff9313524d93bf0f4706fc1
consti <consti@consti.de>
2015-03-26 12:29:34 +0700

    refactor `invite_member` feature helper

--- spec/features/admin/members_spec.rb
+++ spec/features/admin/members_spec.rb
@@ -17,10 +17,10 @@ RSpec.describe 'Admin/Members', type: :feature, js: false do
         expect(page).to have_content(member.first_name)
       end
 
-      # scenario 'viewing dashboard' do
-      #   visit admin_dashboard_path
-      #   expect(page).to have_css('nav.navbar-admin')
-      # end
+      scenario 'viewing dashboard' do
+        visit community_admin_dashboard_path(community)
+        expect(page).to have_css('nav.navbar-admin')
+      end
 
       scenario 'viewing the user menus' do
         visit root_path
@@ -45,10 +45,10 @@ RSpec.describe 'Admin/Members', type: :feature, js: false do
         expect(page).to have_content(member.first_name)
       end
 
-      # scenario 'viewing dashboard' do
-      #   visit admin_dashboard_path
-      #   expect(page).to have_css('nav.navbar-admin')
-      # end
+      scenario 'viewing dashboard' do
+        visit community_admin_dashboard_path(community)
+        expect(page).to have_css('nav.navbar-admin')
+      end
 
       scenario 'viewing the user menus' do
         visit root_path
@@ -87,19 +87,63 @@ RSpec.describe 'Admin/Members', type: :feature, js: false do
   context 'as a Administrator' do
     let!(:administrator) { create(:administrator, :confirmed) }
     let!(:community) { administrator.community }
+
+    let!(:other_community) { create(:community) }
+    let!(:member_of_other_community) do
+      create(:member, :confirmed, community: other_community)
+    end
+
     let!(:company) { create(:company) }
     let!(:position) { create(:position) }
+
+
     feature 'inviting a Regular Member' do
       background do
         as_user administrator
         invite_new_member(
-          'new_member@example.com',
-          first_name: 'Johann',
-          last_name: 'Faust',
-          role: 'Regular Member',
-          community_id: community.id,
-          company: company.name,
-          position: position.name)
+          email: 'new_member@example.com',
+          community: community,
+          attributes: {
+            first_name: 'Johann',
+            last_name: 'Faust',
+            role: 'Regular Member',
+            company: company.name,
+            position: position.name
+          }
+        )
+        sign_out
+      end
+
+      scenario 'Sign up as invited member' do
+        open_email('new_member@example.com')
+        current_email.click_link 'Accept invitation'
+
+        expect(page.find_field('First name').value).to eq 'Johann'
+        expect(page.find_field('Last name').value).to eq 'Faust'
+        expect(page).to_not have_field('Email')
+
+        fill_in 'member[password]', with: 'password0'
+        fill_in 'member[password_confirmation]', with: 'password0'
+        click_button 'Set my password'
+        expect(page).to have_content("Your password was set successfully. You are now signed in.")
+        expect(page).to_not have_css('nav.navbar-admin')
+      end
+    end
+
+    feature 'inviting a Regular Member' do
+      background do
+        as_user administrator
+        invite_new_member(
+          email: 'new_member@example.com',
+          community: community,
+          attributes: {
+            first_name: 'Johann',
+            last_name: 'Faust',
+            role: 'Regular Member',
+            company: company.name,
+            position: position.name
+          }
+        )
         sign_out
       end
 
@@ -123,11 +167,14 @@ RSpec.describe 'Admin/Members', type: :feature, js: false do
       background do
         as_user administrator
         invite_new_member(
-          'new_member@example.com',
-          first_name: 'Johann',
-          last_name: 'Faust',
-          role: 'Administrator',
-          community_id: community.id)
+          email: 'new_member@example.com',
+          community: community,
+          attributes: {
+            first_name: 'Johann',
+            last_name: 'Faust',
+            role: 'Administrator'
+          }
+        )
         sign_out
       end
 
