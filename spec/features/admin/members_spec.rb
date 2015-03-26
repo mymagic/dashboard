e080f338f1e0d326e2ab0fc1ad4e55300cfba413
consti <consti@consti.de>
2015-03-26 12:30:06 +0700

    feature to cover "invite existing member of one
    community to another community"

--- spec/features/admin/members_spec.rb
+++ spec/features/admin/members_spec.rb
@@ -96,6 +96,38 @@ RSpec.describe 'Admin/Members', type: :feature, js: false do
     let!(:company) { create(:company) }
     let!(:position) { create(:position) }
 
+    feature 'inviting a Member who is already member of another community' do
+      background do
+        as_user administrator
+        invite_new_member(
+          email: member_of_other_community.email,
+          community: community,
+          attributes: {
+            first_name: 'Johann',
+            last_name: 'Faust',
+            role: 'Regular Member',
+            company: company.name,
+            position: position.name
+          }
+        )
+        sign_out
+      end
+
+      scenario 'Sign up as invited member' do
+        open_email(member_of_other_community.email)
+        current_email.click_link 'Accept invitation'
+
+        expect(page.find_field('First name').value).to eq 'Johann'
+        expect(page.find_field('Last name').value).to eq 'Faust'
+        expect(page).to_not have_field('Email')
+
+        fill_in 'member[password]', with: 'password0'
+        fill_in 'member[password_confirmation]', with: 'password0'
+        click_button 'Set my password'
+        expect(page).to have_content("Your password was set successfully. You are now signed in.")
+        expect(page).to_not have_css('nav.navbar-admin')
+      end
+    end
 
     feature 'inviting a Regular Member' do
       background do
