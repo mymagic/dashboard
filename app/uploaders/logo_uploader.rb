60786700c363f9f4102dc94f90e18c39a1a04be4
consti <consti@consti.de>
2015-04-01 16:04:37 +0700

    setup cloud storage for images on production

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -3,7 +3,7 @@
 class LogoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
+  storage Rails.configuration.x.uploader_storage
 
   def default_url
     ActionController::Base.helpers.image_path("missing/#{ model.class.to_s.underscore }/" + [version_name, "default.png"].compact.join('_'))
