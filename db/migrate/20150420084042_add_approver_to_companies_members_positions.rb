3256a5a86c44d227da1a1c97de801d39b2aba874
consti <consti@consti.de>
2015-04-22 12:10:01 +0700

    set CompaniesMembersPosition#approver on migration

--- db/migrate/20150420084042_add_approver_to_companies_members_positions.rb
+++ db/migrate/20150420084042_add_approver_to_companies_members_positions.rb
@@ -1,5 +1,14 @@
 class AddApproverToCompaniesMembersPositions < ActiveRecord::Migration
-  def change
+  def up
     add_reference :companies_members_positions, :approver
+    CompaniesMembersPosition.where(approved: true).each do |cmp|
+      admin = cmp.position.community.members.where(role: 'administrator').first
+      cmp.approver = admin
+      cmp.save
+    end
+  end
+
+  def down
+    remove_reference :companies_members_positions, :approver
   end
 end
