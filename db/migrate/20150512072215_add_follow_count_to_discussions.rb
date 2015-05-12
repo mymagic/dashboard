3feae3e77dd41fe2de592df40c1ae804ee6a3064
consti <consti@consti.de>
2015-05-12 17:39:17 +0700

    make migration reversible

--- db/migrate/20150512072215_add_follow_count_to_discussions.rb
+++ db/migrate/20150512072215_add_follow_count_to_discussions.rb
@@ -1,8 +1,12 @@
 class AddFollowCountToDiscussions < ActiveRecord::Migration
-  def change
+  def up
     add_column :discussions, :follows_count, :integer
     Discussion.find_each do |discussion|
       Discussion.reset_counters(discussion.id, :follows)
     end
+  end
+
+  def down
+    remove_column :discussions, :follows_count
   end
 end
