613da89926c4fa19250d9b948e7c0d64a082830f
consti <consti@consti.de>
2015-06-04 15:40:09 +0700

    Allow users to filter unanswered discussions

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -4,7 +4,8 @@ module DiscussionsHelper
     desc << {
       recent: "Most recent discussions",
       popular: "Discussions sorted by amount of followers",
-      hot: "Discussions sorted by amount of followers in the last two weeks"
+      hot: "Discussions sorted by amount of followers in the last two weeks",
+      unanswered: "Unanswered discussions"
     }[filter]
     desc << [", tagged with ", content_tag('strong', tag.name)] if tag
     safe_join(desc) + '.'
