34b2f71f4b2461c08b1e2a5d3b618303bd1991bc
n-thongjor <nut@oozou.com>
2015-06-09 10:30:00 +0700

    Add Reserved by message with participant name

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -2,4 +2,14 @@ module AvailabilitiesHelper
   def view_slots_text(availability)
     availability.member == current_member ? 'View' : 'Reserve'
   end
+
+  def slot_available_message(slot)
+    return 'Available' if slot.available
+    
+    if can? :manage, @availability
+      "Reserved By #{slot.member.try(:full_name)}"
+    else
+      'Unavailable'
+    end
+  end
 end
