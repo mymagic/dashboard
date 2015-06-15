1706800a8dcad138a0a02e2552506df59e90fe56
consti <consti@consti.de>
2015-06-15 11:16:40 +0700

    Event activity

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Events', type: :feature, js: false do
   feature "Administration" do
-    given!(:community) { create(:community, :with_social_media_services) }
+    given!(:community) { create(:community) }
     given!(:administrator) do
       create(:administrator, :confirmed, community: community)
     end
@@ -64,17 +64,43 @@ RSpec.describe 'Events', type: :feature, js: false do
       end
     end
 
+    shared_examples "creating events" do
+      it 'creates an event' do
+        visit community_admin_events_path(community)
+        click_link 'Create new Event'
+
+        fill_in 'Title', with: 'Boston Teaparty'
+        fill_in 'Description', with: 'Free tea!'
+
+        select 'Address', from: 'Location type'
+        fill_in 'Location detail', with: 'Bangkok'
+
+        select '10', from: 'event_starts_at_4i'
+        select '20', from: 'event_ends_at_4i'
+
+        click_button 'Save Event'
+
+        expect(page).to have_content 'Event was successfully created.'
+
+        visit community_path(community)
+        within '.activity-group' do
+          expect(page).to have_content 'setup Boston Teaparty'
+        end
+      end
+    end
 
     context 'as administrator' do
       background { as_user administrator }
       it_behaves_like "viewing events"
       it_behaves_like "rsvp events"
+      it_behaves_like "creating events"
     end
 
     context 'as staff' do
       background { as_user staff }
       it_behaves_like "viewing events"
       it_behaves_like "rsvp events"
+      it_behaves_like "creating events"
     end
 
     context 'as manager' do
