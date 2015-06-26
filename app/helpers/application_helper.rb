1407cacd5b99d7f62af7f2796d77dfa35c2ce8a3
consti <consti@consti.de>
2015-06-26 11:08:25 +0700

    Fix favicon url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def favicon_path
-    return current_community.logo.url if current_community
+    return current_community.logo.url(:icon) if current_community
     image_path('missing/community/icon_default.png')
   end
 end
