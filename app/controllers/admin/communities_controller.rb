8317c8c7fe6b040c3a6a9625f9e4b49c825c994c
consti <consti@consti.de>
2015-08-14 10:00:14 +0700

    redirect back to Admin/Communtiy/Edit on Admin edit

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -10,7 +10,7 @@ module Admin
     def update
       if @current_community.update_attributes(community_params)
         redirect_to(
-          community_path(@current_community),
+          edit_community_admin_community_path(@current_community),
           notice: 'Settings were successfully updated.')
       else
         render 'edit', alert: 'Error updating community.'
