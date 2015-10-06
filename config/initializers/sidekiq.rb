5bdc090efacaf7ba1ca50a216288f7a15bbafe92
consti <consti@consti.de>
2015-10-06 17:14:23 +0700

    Reduce Sidekiq concurrency to 17

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
+Sidekiq.configure_client do |config|
+  config.redis = { size: 1 }
+end
+
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Middleware::Server::RetryJobs, max_retries: 3
